68f1dbad9d3aa560af7371d3e47f60a813ea525a
Sumit Kumar <sumitkmr130499@gmail.com>
2023-09-15 12:58:35 +0530

    authentication

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const connectDb = async ()=>{
+  try{
+      const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+      console.log("Connected Successfully", connect.connection.host,connect.connection.name);
+  }catch(err){
+      console.log(err);
+      process.exit(1);
+  }
+}
+connectDb();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const { registerUser, loginUser, currentUserInfo,courseOne,courseTwo } = require("../controllers/userController");
+const validateToken = require("../middleware/validateTokenHandler");
+
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.post("/register",registerUser);
+
+router.post("/login",loginUser);
+
+router.get("/current",validateToken,currentUserInfo);
+router.get("/courseOne",validateToken,courseOne);
+router.get("/courseTwo",validateToken,courseTwo);
 
 module.exports = router;
